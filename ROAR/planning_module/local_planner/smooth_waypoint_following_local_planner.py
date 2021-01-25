a265aed86b6b904af8cd158c743b278c3b401d2f
Alfredo De Goyeneche <asdegoyeneche@gmail.com>
2021-01-24 19:59:44 -0800

    cleanup SmoothWaypointLookahead class

--- ROAR/planning_module/local_planner/smooth_waypoint_following_local_planner.py
+++ ROAR/planning_module/local_planner/smooth_waypoint_following_local_planner.py
@@ -1,6 +1,8 @@
 from functools import reduce
 from typing import Union
 
+import numpy as np
+
 from ROAR.planning_module.local_planner.simple_waypoint_following_local_planner import \
     SimpleWaypointFollowingLocalPlanner
 from ROAR.utilities_module.data_structures_models import Transform
@@ -8,33 +10,35 @@ from ROAR.utilities_module.errors import (
     AgentException,
 )
 from ROAR.utilities_module.vehicle_models import Vehicle, VehicleControl
-import numpy as np
 
 
 class SmoothWaypointFollowingLocalPlanner(SimpleWaypointFollowingLocalPlanner):
+    """
+    Waypoint following local planner with waypoint lookahead for smoothing and target speed reduction.
+    """
 
-    def next_waypoint_smooth_and_speed(self, smooth_factor=400, speed_lookahead=600) -> (Transform, float):
-        # car_speed = np.linalg.norm(self.agent.vehicle.velocity.to_array()) * 3.6  # m/s to km/hr
-        # lookahead_multiplier = car_speed / 170  # self.agent.agent_settings.target_speed
-        #
-        # waypoint_lookahead = max(waypoint_lookahead // 2, int(waypoint_lookahead * lookahead_multiplier))
-        # speed_lookahead = max(speed_lookahead // 2, int(speed_lookahead * lookahead_multiplier))
-        #
-        # waypoint_lookahead = min(waypoint_lookahead, len(self.way_points_queue) - 1)
-        # speed_lookahead = min(speed_lookahead, len(self.way_points_queue) - 1)
-        #
-        # print("WAYPOINT", waypoint_lookahead)
-        #
-        # if waypoint_lookahead > 1:  # can get rid of if statement here
-        #     target_waypoint = self.way_points_queue[waypoint_lookahead]
-        # else:
-        #     target_waypoint = self.way_points_queue[-1]
+    def next_waypoint_smooth_and_speed(self, smooth_lookahead=400, speed_lookahead=600) -> (Transform, float):
+        """
+        Calculate the next target waypoint and speed for the controller.
 
-        smooth_factor = min(smooth_factor, len(self.way_points_queue) - 1)
+        Parameters
+        smooth_lookahead : int
+            Number of waypoints ahead to look at to compute the smoothed waypoint.
+        speed_lookahead : int
+            Number of waypoint to look ahaed to compute speed factor.
+
+        Returns
+        target_waypoint : Transform
+            The next target waypoint for the controller
+        speed_multiplier : float
+            The speed multiplier for the controller's target speed.
+        """
+
+        smooth_lookahead = min(smooth_lookahead, len(self.way_points_queue) - 1)
         speed_lookahead = min(speed_lookahead, len(self.way_points_queue) - 1)
 
-        if smooth_factor > 10:
-            sample_points = range(0, smooth_factor, smooth_factor//10)
+        if smooth_lookahead > 10:  # Reduce computation by only looking at every 10 steps ahead
+            sample_points = range(0, smooth_lookahead, smooth_lookahead // 10)
             location_sum = reduce(lambda x, y: x + y,
                                   (self.way_points_queue[i].location for i in sample_points))
             rotation_sum = reduce(lambda x, y: x + y,
@@ -45,41 +49,13 @@ class SmoothWaypointFollowingLocalPlanner(SimpleWaypointFollowingLocalPlanner):
         else:
             target_waypoint = self.way_points_queue[-1]
 
-        # angle_std = np.std([self.way_points_queue[i].rotation.yaw for i in sample_points])
-        # # speed_multiplier = 1 - angle_std
-        # speed_multiplier = 1.0
-        # print()
-        # print([round(self.way_points_queue[i].rotation.yaw, 2) for i in sample_points])
-        # print([round(self.way_points_queue[i].rotation.pitch, 2) for i in sample_points])
-
-        # mean_pitch_0 = (self.way_points_queue[0].rotation.pitch + self.way_points_queue[1].rotation.pitch) / 2
-        # mean_pitch_ahead = (self.way_points_queue[speed_lookahead - 1].rotation.pitch +
-        #                     self.way_points_queue[speed_lookahead].rotation.pitch) / 2
-        # angle_difference = abs(np.deg2rad(mean_pitch_0 - mean_pitch_ahead) % (2 * np.pi))
-        # angle_difference = min((2 * np.pi) - angle_difference, angle_difference)
-        #
-        # speed_multiplier = (1.0 - angle_difference / np.pi)
-
         if speed_lookahead > 0:
-            # print(speed_lookahead, len(self.way_points_queue))
             angle_difference = self._calculate_angle_error(self.way_points_queue[speed_lookahead])
             # Angle difference is between 0 and 180, but unlikely to be more than 90
             speed_multiplier = max(0.6, (1.0 - 1.3 * angle_difference / np.pi))
-            # speed_multiplier = np.exp(- 2.0 * angle_difference) * 0.5 + 0.5
-            # speed_multiplier = max(0.5, (1.0 - angle_difference / np.pi) ** 1.5)
-            # speed_multiplier = max(0.5, (2.0 - (1.0 + angle_difference) ** 2))
 
-            # print("Angle difference:", np.degrees(angle_difference))
-            # print("Speed Multiplier", speed_multiplier)
         else:
             speed_multiplier = 0.0
-
-        # angles = []
-        # for index in (speed_lookahead, speed_lookahead // 5):
-        #     delta_x = self.way_points_queue[index].location.x - self.way_points_queue[0].location.x
-        #     delta_z = self.way_points_queue[index].location.z - self.way_points_queue[0].location.z
-        #     angles.append(np.arctan2(delta_z, delta_x))
-        # delta_angle = angles[0] - anglessum
 
         return target_waypoint, speed_multiplier
 
@@ -139,8 +115,19 @@ class SmoothWaypointFollowingLocalPlanner(SimpleWaypointFollowingLocalPlanner):
                           f"Control: {control} | Speed: {Vehicle.get_speed(self.agent.vehicle)}\n")
         return control
 
-    def _calculate_angle_error(self, next_waypoint: Transform, epsilon=10):
-        # calculate a vector that represent where you are going
+    def _calculate_angle_error(self, next_waypoint: Transform):
+        """
+        Compute the angle difference between the current vehicle position and orientation and the specified waypoint.
+        Ignore the height component (y).
+
+        Parameters
+        next_waypoint : Transform
+            The waypoint to compute the angle against.
+
+        Returns
+        : float
+            The angle difference in radians in the 0 to pi range.
+        """
 
         v_vec = np.array([np.cos(np.radians(self.agent.vehicle.transform.rotation.pitch)),
                           0,
@@ -151,7 +138,4 @@ class SmoothWaypointFollowingLocalPlanner(SimpleWaypointFollowingLocalPlanner):
         w_vec[1] = 0.
         w_vec_norm = np.linalg.norm(w_vec)
 
-        # print("NORM", round(w_vec_norm, 3), round(v_vec_norm, 3))
-        # if w_vec_norm > epsilon:
         return np.math.acos(np.dot(v_vec, w_vec) / (v_vec_norm * w_vec_norm))  # 0 to np.pi
-        # return 0.0
